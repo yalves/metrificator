afd80307011139b71967a0cdb726848218b97760
Yan Alves <yanalves@hotmail.com>
2021-05-28 09:34:58 -0300

    Add disk view

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, disk
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('disk', disk)
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 import psutil
 
 def index(request):
-    memory = psutil.virtual_memory()
-    context = {
-        'memory': memory.percent,
-    }
-    return render(request, 'index.html', context)
+  memory = psutil.virtual_memory()
+  context = {
+      'memory': memory.percent,
+  }
+  return render(request, 'index.html', context)
 
-    # return render(request, 'index.html', {})
+def disk(request):
+  memory = psutil.virtual_memory()
+  disk = psutil.disk_usage('.')
+  context = {
+      'disk_total': round(disk.total/(1024*1024*1024), 2),
+      'disk_used': round(disk.used/(1024*1024*1024), 2),
+      'disk_free': round(disk.free/(1024*1024*1024), 2),
+  }
+  return render(request, 'disk.html', context)
